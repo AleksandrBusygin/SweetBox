f07fbca6cbcf38db39c118f1fbf9b795f2a123ee
Aleksandr Busygin <persik665@yandex.ru>
2019-03-09 14:13:16 +0300

    доработка

--- src/HomeWork/Sweet.java
+++ src/HomeWork/Sweet.java
@@ -1,16 +1,17 @@
 package HomeWork;
 
 public abstract class Sweet {
+    String name;
     double price, weight;
 
-    public Sweet(double price, double weight) {
+    public Sweet(String name, double price, double weight) {
+        this.name = name;
         this.price = price;
         this.weight = weight;
     }
-
     @Override
     public String toString() {
-        return "Тип сладкого подарка: ";
+        return "Тип сладкого подарка: " + name;
     }
 }
 
